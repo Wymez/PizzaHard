c0c84d04a0d468c2612a1f46654acc2f223aea34
Wymez <anton.fuflygin@gmail.com>
2021-10-22 14:42:33 +0300

    header menu

--- account/views.py
+++ account/views.py
@@ -1,6 +1,6 @@
 import json
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
 from django.contrib.auth.decorators import login_required
@@ -9,10 +9,11 @@ from django.contrib.auth.forms import UserCreationForm
 from django.http import JsonResponse
 
 
+# def index_page(reque
 @login_required
 def dashboard(request):
-    # return render(request, 'account/dashboard.html', {'section': dashboard})
-    return JsonResponse()
+    return render(request, 'account/dashboard.html', {'section': dashboard})
+    # return JsonResponse()
 
 
 def register(request):
@@ -20,8 +21,13 @@ def register(request):
         user_form = UserCreationForm(request.POST)
         if user_form.is_valid():
             new_user = user_form.save(commit=False)
-            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.set_password(user_form.cleaned_data['password1'])
             new_user.save()
+            cd = user_form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password1'])
+            if user is not None:
+                login(request, user)
+                return redirect('dashboard')
     else:
         user_form = UserCreationForm()
     return render(request, 'account/register.html', {'user_form': user_form})
