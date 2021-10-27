9cd7ce68df0afe05baee489cddaf4f61286451da
Wymez <anton.fuflygin@gmail.com>
2021-10-27 15:15:54 +0300

    webpack tuned in

---
+++ static/js/main.js
@@ -0,0 +1,6 @@
+import "bootstrap/dist/css/bootstrap.css"
+
+import "../css/styles.css"
+
+import "./pizza-sorts.js"
+import "./header-menu.js"
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,41 @@
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'templates'),
+  entry: './static/js/main.js',
+  resolve: {
+    extensions: [".js", ".jsx"],
   },
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'static/dist'),
+    publicPath: '/static/dist/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/env", "@babel/react"],
+          plugins: [],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: path.join(__dirname, "src/base.html"),
+        filename: "../../templates/base.html"
+    }),
+    new MiniCssExtractPlugin(),
+  ],
 };
